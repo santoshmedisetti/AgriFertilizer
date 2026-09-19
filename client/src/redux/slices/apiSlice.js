4ebb19584c997c32615aa113776c74e2c5844b78
santoshmedisetti <medisettisantosh7@gmail.com>
2026-09-19 21:11:12 +0530

    Change API base URL to production endpoint

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { logout } from './authSlice';
 
 const baseQuery = fetchBaseQuery({ 
-  baseUrl: 'http://localhost:5000',
+  baseUrl: 'https://agrifertilizer.onrender.com',
   credentials: 'include',
 });
 
